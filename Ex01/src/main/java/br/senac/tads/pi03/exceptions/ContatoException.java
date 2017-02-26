59ac877576bb26e859b16dd09eb1af5a264878e5
Danilo Kenzo <dani.kenzo@hotmail.com>
2017-02-25 22:29:48 -0300

    Exception e atributos

---
+++ Ex01/src/main/java/br/senac/tads/pi03/exceptions/ContatoException.java
@@ -0,0 +1,11 @@
+package br.senac.tads.pi03.exceptions;
+
+public class ContatoException extends Exception {
+
+    /**
+     * Construtor de exceções que permite informar uma mensagem *
+     */
+    public ContatoException(String message) {
+        super(message);
+    }
+}
